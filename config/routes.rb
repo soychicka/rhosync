c289a1ad69f3f85e0530865c6a05d220a016ea12
lars <larsburgess@gmail.com>
2009-06-26 14:21:37 -0700

    updating route for test page

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'apps/:app_id/sources/:id/createobjects', :controller => 'sources', :action => 'createobjects'
   map.connect 'apps/:app_id/sources/:id/deleteobjects', :controller => 'sources', :action => 'deleteobjects'
   map.connect 'apps/:app_id/sources/:id/ask', :controller => 'sources', :action => 'ask'
+  map.connect 'apps/:app_id/sources/:id/test_createobjects', :controller => 'sources', :action => 'test_createobjects'
 
   map.connect 'sources/:id/refresh', :controller => 'sources', :action => 'refresh'
   map.connect 'apps/:app_id/sources/:id/refresh', :controller => 'sources', :action => 'refresh'
