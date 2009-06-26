0dac8af985952091031924a084ea900a8ad7c928
adamblum <a@adamblum.com>
2009-06-25 19:37:45 -0700

    changed the inheritance column to be "device_type" due to baffling reports of somehow "type" being an invalid column name?!

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,7 @@
 class Device < ActiveRecord::Base
   #  belongs_to :source  DON'T NEED THIS NOW. Can just say that devices belong to users
   belongs_to :user
+  set_inheritance_column "device_type"
   
   def ping(callback_url,message=nil,vibrate=nil)  # this should never get hit
     raise "Base device class notify.  Should never hit this!"
