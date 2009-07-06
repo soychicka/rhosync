018fdce316f7a46aa7fccb49e4e3aebeb0d45d8d
Vidal Graupera <vgraupera@gmail.com>
2009-07-06 15:04:06 -0700

    some fixes to job script

--- jobs/sync_and_ping_user.rb
+++ jobs/sync_and_ping_user.rb
@@ -1,9 +1,21 @@
+File.join(File.dirname(__FILE__), '..', 'lib', 'source_adapter.rb')
+
 # usage: sync_and_ping_user.rb TUser 10 http://rhosync.example.com/sources/10/show
-current_user=User.find_by_login(ARGV[0])
+current_user=User.find(ARGV[0])
 source=Source.find_by_permalink(ARGV[1])
 callback_url=ARGV[2]
 
-puts "starting bj job at #{Time.now}"
+Rails.logger.debug "****************"
+Rails.logger.debug "#{Time.now} starting sync_and_ping_user #{ARGV.inspect.to_s}"
+Rails.logger.debug "current_user = #{current_user.inspect.to_s}"
+Rails.logger.debug "source = #{source.inspect.to_s}"
+Rails.logger.debug "callback_url = #{callback_url}"
 
-source.dosync(current_user)
-current_user.ping(callback_url)
+begin
+  source.dosync(current_user)
+  current_user.ping(callback_url)
+rescue SourceAdapterLoginException
+  current_user.ping(callback_url, "login failed")
+end
+
+Rails.logger.debug "... done sync_and_ping_user #{Time.now}"
