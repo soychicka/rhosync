803e8f23ac68cfc0bf0a32926f885db4c0b3395d
Vidal Graupera <vgraupera@gmail.com>
2009-07-06 12:02:39 -0700

    fix to run on windows

--- jobs/sync_and_ping_user.rb
+++ jobs/sync_and_ping_user.rb
@@ -1,7 +1,9 @@
 # usage: sync_and_ping_user.rb TUser 10 http://rhosync.example.com/sources/10/show
 current_user=User.find_by_login(ARGV[0])
-source=Source.find(ARGV[1])
+source=Source.find_by_permalink(ARGV[1])
 callback_url=ARGV[2]
+
+puts "starting bj job at #{Time.now}"
 
 source.dosync(current_user)
 current_user.ping(callback_url)
