4faafbedc06b1aee4722ac05240e0f86fd733a37
Vidal Graupera <vidal@vdggroup.com>
2009-07-30 14:39:27 -0700

    re-add line that got lost in merge

--- config/environment.rb
+++ config/environment.rb
@@ -34,7 +34,8 @@ Rails::Initializer.run do |config|
   config.gem "rspec-rails", :lib => "spec/rake/spectask"
   config.gem "rcov"
   config.gem "libxml-ruby", :lib => "xml/libxml"
-  
+
+  config.gem "datanoise-actionwebservice", :lib => "actionwebservice", :version => "2.2.2"  
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
