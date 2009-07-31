8f94cf49f10f130acd0bdb38b364ed0097198a0d
Vidal Graupera <vidal@vdggroup.com>
2009-07-31 09:59:19 -0700

    remove limit of 255 on values which was merged by mistake

--- db/schema.rb
+++ db/schema.rb
@@ -140,7 +140,7 @@ ActiveRecord::Schema.define(:version => 20090716233709) do
     t.integer  "source_id"
     t.string   "object"
     t.string   "attrib"
-    t.text     "value",             :limit => 255
+    t.text     "value"
     t.integer  "pending_id"
     t.string   "update_type"
     t.integer  "user_id"
