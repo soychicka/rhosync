b2bd83bfd6865a9c9f0b351e61022343dc7d445f
lars <larsburgess@gmail.com>
2009-10-07 11:50:10 -0700

    grab id from client [#255]

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -202,7 +202,7 @@ class SourcesController < ApplicationController
       # add the created ID + created_at time to the list
       objects[o.id]=o.created_at if not objects.keys.index(o.id)  # add to list of objects
       
-      ClientMap.create!(:client_id => @client.client_id, :object_value_id => o.id)
+      ClientMap.create!(:client_id => @client.client_id, :object_value_id => x['id'])
     end
     respond_to do |format|
       if params[:no_redirect]
