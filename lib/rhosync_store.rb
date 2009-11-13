625acfc6e8d32908fcad5e6a3a08e6a26008f825
lars <larsburgess@gmail.com>
2009-11-13 12:02:30 -0800

    added get_diff_data for computing updates

--- lib/rhosync_store.rb
+++ lib/rhosync_store.rb
@@ -17,23 +17,23 @@ class RhosyncStore
       object_set = _setkey(doctype,source,user)
       object_id_set = "#{object_set}:ids"
       _delete_keys("#{object_set}*")
-      now = Time.now.to_i
       data.each do |key,value|
         @db.sadd(object_id_set, key)
         value.each do |attrib,value|
-          @db.sadd(object_set,_setelement(key,attrib,value,now))
+          @db.sadd(object_set,_setelement(key,attrib,value))
         end
       end
+      @db.set(_key_timestamp(doctype,source,user), (Time.now.to_f * 1000).to_i)
     end
     true
   end
   
-  # Retrieves set for given source,user
-  def get_data(doctype,source,user,set=nil)
+  # Retrieves set for given doctype,source,user
+  def get_data(doctype,source,user)
     res = {}
     if doctype and source and user
       @db.smembers(_setkey(doctype,source,user)).each do |element|
-        key,attrib,value,timestamp = _getelement(element)
+        key,attrib,value = _getelement(element)
         res[key] = {} unless res[key]
         res[key].merge!({attrib => value})
       end
@@ -41,8 +41,27 @@ class RhosyncStore
     end
   end
   
+  # Retrieves diff data hash between two sets
+  def get_diff_data(srcdoc,dstdoc,source,user)
+    res = {}
+    if srcdoc and dstdoc and source and user
+      @db.sdiff(_setkey(dstdoc,source,user),_setkey(srcdoc,source,user)).each do |element|
+        key,attrib,value = _getelement(element)
+        res[key] = {} unless res[key]
+        res[key].merge!({attrib => value})
+      end
+    end
+    res
+  end
+  
+  # Returns timestamp (integer) for given doctype,source,user
+  def get_timestamp(doctype,source,user)
+    ts = @db.get(_key_timestamp(doctype,source,user))
+    ts ? ts.to_i : nil
+  end
+  
   # Compute difference between two sets
-  def get_deleted(srcdoc,dstdoc,source,user)
+  def get_diff_ids(srcdoc,dstdoc,source,user)
     res = []
     if srcdoc and dstdoc and source and user
       res = @db.sdiff(_setkey_ids(dstdoc,source,user),_setkey_ids(srcdoc,source,user))
@@ -58,14 +77,18 @@ class RhosyncStore
   def _setkey_ids(doctype,source,user)
     "#{_setkey(doctype,source,user)}:ids"
   end
-  
-  def _setelement(obj,attrib,value,timestamp)
-    "#{obj}:#{attrib}:#{Base64.encode64(value)}:#{timestamp}"
+
+  def _setelement(obj,attrib,value)
+    "#{obj}:#{attrib}:#{Base64.encode64(value)}"
   end
   
   def _getelement(element)
     res = element.split(':')
-    [res[0], res[1], Base64.decode64(res[2]), res[3]]
+    [res[0], res[1], Base64.decode64(res[2])]
+  end
+  
+  def _key_timestamp(doctype,source,user)
+    "#{_setkey(doctype,source,user)}:ts"
   end
   
   def _delete_keys(keymask)
--- spec/rhosync_store_spec.rb
+++ spec/rhosync_store_spec.rb
@@ -37,8 +37,10 @@ describe "RhosyncStore" do
   end
   
   it "should add simple data to new set" do
-    @sync_store.put_data('md',@source,@user,@data).should == true
-    @sync_store.get_data('md',@source,@user).should == @data
+    start = (Time.now.to_f * 1000).to_i
+    @sync_store.put_data('doc1',@source,@user,@data).should == true
+    @sync_store.get_timestamp('doc1',@source,@user).should >= start
+    @sync_store.get_data('doc1',@source,@user).should == @data
   end
   
   it "should replace simple data to existing set" do
@@ -48,51 +50,36 @@ describe "RhosyncStore" do
     @sync_store.get_data('doc1',@source,@user).should == new_data
   end
   
-  it "should return doc1 objects that were deleted in doc2" do
+  it "should return ids that were deleted in doc2" do
     @data1,@data1['1'],@data1['2'] = {},@product1,@product2
     expected = ['3']
     @sync_store.put_data('doc1',@source,@user,@data).should == true
     @sync_store.get_data('doc1',@source,@user).should == @data
     @sync_store.put_data('doc2',@source,@user,@data1)
     @sync_store.get_data('doc2',@source,@user).should == @data1
-    @sync_store.get_deleted('doc2','doc1',@source,@user).should == expected
+    @sync_store.get_diff_ids('doc2','doc1',@source,@user).should == expected
   end
   
-  # it "should return new records" do
-    # @data1,@data1['1'],@data1['2'],@data1['3'] = {},@product1,@product2,@product4
-    # @sync_store.put_data('md',@source,@user,@data).should == true
-    # @sync_store.get_data('md',@source,@user).should == @data
-    # @sync_store.put_data('bd',@source,@user,@data1)
-    # @sync_store.get_data('bd',@source,@user).should == @data1
-  #   
-  #   result,result['3'] = {}, @product4
-  #   @sync_store.get_diff('md','bd',@source,@user).should == result
-  #   
-  #   # result,result['3'] = {}, @product3
-  #   # @sync_store.get_diff('bd','md',@source,@user).should == result
-  # end
+  it "should return ids that were created in doc2" do
+    @data1,@data1['1'],@data1['2'],@data1['3'],@data['4'] = {},@product1,@product2,@product3,@product4
+    expected = ['4']
+    @sync_store.put_data('doc1',@source,@user,@data).should == true
+    @sync_store.get_data('doc1',@source,@user).should == @data
+    @sync_store.put_data('doc2',@source,@user,@data1)
+    @sync_store.get_data('doc2',@source,@user).should == @data1
+    @sync_store.get_diff_ids('doc2','doc1',@source,@user).should == expected
+  end
   
-  # it "should return obsolete records" do
-  #   
-  # end
-  # 
-  # it "should return modified records" do
-  #   
-  # end
-  
-  
-  # 
-  # it "should compute backend shadow copy" do
-  #   
-  # end
-  
-  # 
-  # it "should compute intersect between two sets" do
-  #   
-  # end
-  # 
-  # it "should retrieve all keys for a set" do
-  #   
-  # end
-  
+  it "should return attributes modified in doc2" do
+    @sync_store.put_data('doc1',@source,@user,@data).should == true
+    @sync_store.get_data('doc1',@source,@user).should == @data
+    
+    @product3['price'] = '59.99'
+    expected = { '3' => { 'price' => '59.99' } }
+    @data1,@data1['1'],@data1['2'],@data1['3'] = {},@product1,@product2,@product3
+    
+    @sync_store.put_data('doc2',@source,@user,@data1)
+    @sync_store.get_data('doc2',@source,@user).should == @data1
+    @sync_store.get_diff_data('doc1','doc2',@source,@user).should == expected
+  end
 end
