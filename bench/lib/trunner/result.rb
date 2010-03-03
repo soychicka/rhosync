46fae689333637e9d4e065ffa1bca00db1fb70c6
vtrv101 <vlad@vlads-macbook-pro-2.local>
2010-03-02 17:54:13 -0800

    completed spec for Trunner::Result

--- bench/lib/trunner/result.rb
+++ bench/lib/trunner/result.rb
@@ -27,12 +27,14 @@ module Trunner
     end
     
     def verify_body(expected)
-      expected,actual = JSON.parse(expected),JSON.parse(@last_response)
+      expected,actual = JSON.parse(expected),JSON.parse(@last_response.to_s)
       if expected != actual
         logger.error "#{log_prefix} Verify error at: " + caller(1)[0].to_s
-        logger.error "#{log_prefix} Diff: "
-        Result.compare(expected,actual).each do |diff|
-          logger.error "#{log_prefix} Path: #{diff[:path].join('/')} \nLValue:\t#{diff[:lvalue].inspect}\nRValue:\t#{diff[:rvalue].inspect}"
+        logger.error "#{log_prefix} Message diff: "
+        Result.compare(:expected,expected,:actual,actual).each do |diff|
+          logger.error "#{log_prefix} Path: #{diff[:path].join('/')}"
+          logger.error "#{log_prefix} Expected: #{diff[:expected].inspect}"
+          logger.error "#{log_prefix} Actual: #{diff[:actual].inspect}"
         end
         #logger.error "#{log_prefix} expected:\n#{expected.inspect}\n but got:\n#{actual.inspect}"
         @verification_error = true
@@ -42,7 +44,9 @@ module Trunner
     def verify_code(expected)
       if expected != @last_response.code
         logger.error "#{log_prefix} Verify error at: " + caller(1)[0].to_s
-        logger.error "#{log_prefix} expected: #{expected.inspect}\n but got: #{@last_response.code}"
+        logger.error "#{log_prefix} Code diff: "
+        logger.error "#{log_prefix} expected: #{expected.inspect}"
+        logger.error "#{log_prefix} but got:  #{@last_response.code}"
         @verification_error = true
       end
     end
@@ -51,13 +55,13 @@ module Trunner
       
     end
     
-    def self.compare(s1,s2)
-      r1 = diff([],s1,s2)
-      r2 = diff([],s2,s1)
+    def self.compare(name1,s1,name2,s2)
+      r1 = diff([],name1,s1,name2,s2)
+      r2 = diff([],name2,s2,name1,s1)
       r1.size > r2.size ? r1 : r2
     end
     
-    def self.diff(res,lvalue,rvalue,path=[])
+    def self.diff(res,lname,lvalue,rname,rvalue,path=[])
 
       return res if lvalue == rvalue
       
@@ -65,16 +69,16 @@ module Trunner
         lvalue.each_index do |index| 
           p = Array.new(path)
           p << index
-          diff(res,lvalue[index],rvalue.at(index),p)
+          diff(res,lname,lvalue[index],rname,rvalue.at(index),p)
         end                
       elsif lvalue.is_a?(Hash) and rvalue
         lvalue.each do |key,value| 
           p = Array.new(path)
           p << key
-          diff(res,value,rvalue[key],p)
+          diff(res,lname,value,rname,rvalue[key],p)
         end
       else            
-        res << {:path=>path,:lvalue=>lvalue,:rvalue=>rvalue}
+        res << {:path=>path,lname=>lvalue,rname=>rvalue}
       end
       
       res 
--- bench/spec/result_spec.rb
+++ bench/spec/result_spec.rb
@@ -4,28 +4,55 @@ require 'rest_client'
 
 describe "ResultSpec" do
   it_should_behave_like "TrunnerSpecHelper"
-  
-  it "should compare two arrays" do
-    s1 = [{"foo" => {"bar" => "cake"}}]
-    s2 = [{"foo" => {"bar" => "cake1"}},{"hello" => "world"}]
-    Result.compare(s1,s2).should == [{:rvalue=>"cake", 
-      :path=>[0, "foo", "bar"], :lvalue=>"cake1"}, 
-      {:rvalue=>nil, :path=>[1], :lvalue=>{"hello"=>"world"}}]
+
+  before(:each) do
+    @s1 = [{"foo" => {"bar" => "cake"}}]
+    @s2 = [{"foo" => {"bar" => "cake1"}},{"hello" => "world"}]
+    @result = Result.new("marker",:get,"some/url",0,1)
+    client = mock("RestClient")
+    client.stub!(:headers).and_return({'header1'=>'headervalue1'})
+    client.stub!(:cookies).and_return({'session1'=>'sessval1'})
+    client.stub!(:code).and_return(200)
+    client.stub!(:to_s).and_return(@s1.to_json)
+    @result.last_response = client
   end
   
-  # it "should verify_body" do
-  #   @headers = {'header1'=>'headervalue1'}
-  #   @cookies = {'session1'=>'sessval1'}
-  #   @code = 200
-  #   @body = '[{"version":3}]'
-  #   client = mock("RestClient")
-  #   client.stub!(:headers).and_return(@headers)
-  #   client.stub!(:cookies).and_return(@cookies)
-  #   client.stub!(:code).and_return(@code)
-  #   client.stub!(:body).and_return(@body)
-  #   r = Result.new('hello','get','/hello',1,1)
-  #   r.stub!(:last_response).and_return(client)
-  #   r.verify_body(@body).should == nil
-  #   puts "last: #{r.last_response.inspect}"
-  # end
+  describe "test @last_response wrapper" do
+  
+    it "shold return 'code'" do
+      @result.code.should == 200
+    end
+
+    it "shold return 'body'" do
+      JSON.parse(@result.body).should == @s1
+    end
+    
+    it "shold return 'cookies'" do
+      @result.cookies.should == {'session1'=>'sessval1'}
+    end
+    
+    it "shhould return 'headers'" do
+      @result.headers.should == {'header1'=>'headervalue1'}
+    end
+    
+  end
+  
+  it "should compare two array/hash structures" do
+    Result.compare(:expected,@s1,:actual,@s2).should == [{:expected=>"cake", 
+      :path=>[0, "foo", "bar"], :actual=>"cake1"}, 
+      {:expected=>nil, :path=>[1], :actual=>{"hello"=>"world"}}]
+  end
+  
+  it "should verify body" do
+    @result.logger.should_receive(:error).exactly(8).times
+    @result.verify_body(@s2.to_json)
+    @result.verification_error.should == true
+  end  
+
+  it "should verify code" do
+    @result.logger.should_receive(:error).exactly(4).times
+    @result.verify_code(500)
+    @result.verification_error.should == true
+  end
+      
 end
